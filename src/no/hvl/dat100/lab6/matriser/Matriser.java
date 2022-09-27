7c5c30b6c713ca845cad562de3fa1e15c97951b9
Bjørnar <598062@stud.hvl.no>
2022-09-27 10:47:41 +0200

    :sparkles: lagt til en test matrise

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -6,29 +6,39 @@ public class Matriser {
 
 	public static void main(String[] args) {
 		int[][] arr = new int[10][20];
-        arr[0][0] = 1;
-        
-        skrivUt(arr);
+		arr[0][0] = 1;
+
+		int[][] matrise8 = {{0, 0, 1, 1, 1, 1, 0, 0},
+		                    {0, 1, 0, 0, 0, 0, 1, 0},
+		                    {0, 1, 0, 0, 0, 0, 1, 0},
+		                    {0, 0, 1, 1, 1, 1, 0, 0},
+		                    {0, 1, 0, 0, 0, 0, 1, 0},
+		                    {0, 1, 0, 0, 0, 0, 1, 0},
+		                    {0, 1, 0, 0, 0, 0, 1, 0},
+		                    {0, 0, 1, 1, 1, 1, 0, 0},};
+
+		skrivUt(arr);
+		speile(matrise8);
 	}
-	
+
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
+
 		// TODO
 		// throw new UnsupportedOperationException("skrivUt ikke implementert");
 		System.out.println("-------------------------------");
 
 		for (int[] rad : matrise) {
-			
+
 			for (int i : rad) {
 				System.out.print(i + " ");
 			}
-			
+
 			System.out.println();
 		}
 
 		System.out.println("-------------------------------");
-		
+
 	}
 
 	// b)
@@ -41,7 +51,8 @@ public class Matriser {
 			for (int tall : tabell) {
 				out.append(tall);
 				out.append(" ");
-;			}
+				;
+			}
 			out.append("\n");
 		}
 		System.out.println(out);
@@ -50,7 +61,7 @@ public class Matriser {
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
+
 		// TODO
 		// throw new UnsupportedOperationException("skaler ikke implementert");
 		// ikke optimalt, denne bør finne ut lengden til hver av under arrays og sette hver lik ti ldenne lengden
@@ -69,13 +80,13 @@ public class Matriser {
 		// TODO
 		throw new UnsupportedOperationException("erLik ikke implementert");
 	}
-	
+
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
 		// TODO
 		throw new UnsupportedOperationException("speile ikke implementert");
-	
+
 	}
 
 	// f)
@@ -83,6 +94,6 @@ public class Matriser {
 
 		// TODO
 		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
+
 	}
 }
